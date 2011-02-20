82fedc1a099667940ad7aa8a561ab474352b3b6a
Steven Heidel <steven@livingskyweb.ca>
2011-02-19 20:54:57 -0600

    Copied resources, linked up _head, and got menu working.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CalvaryUnited::Application.config.secret_token = '563404223ea29f262b243b9dea77e40995aae84be82866b39b7fbc05579861e1a1691781ddaf10255b20907506adeb5ec19ee3b9b6104049dd23f202f6f83a4c'
